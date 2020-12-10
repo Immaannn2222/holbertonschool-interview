ab9fc36e0059bd4ff9e2e7f20933d3be13f10876
Immaannn2222 <imennaayari@gmail.com>
2020-12-10 22:38:58 +0100

    coreect

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,10 +4,10 @@
 
 def canUnlockAll(boxes):
     """ determines if all the boxes can be opened"""
-    if boxes[0] is None:
-        return False
     res = []
-    res.append(boxes[0])
+    res.append(0)
+    if boxes[0] == []:
+        return False
     for i in range(len(boxes)):
         for j in boxes[i]:
             if j not in res and j < len(boxes) and j != i:
