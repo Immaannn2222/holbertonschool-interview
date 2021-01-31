d4ebdd55cc5a5b4554c2e92b8ca49c56f7caedf8
Immaannn2222 <imennaayari@gmail.com>
2021-01-31 23:01:56 +0100

    r

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import sys
+"""Log parsing"""
 
+
+import sys
 satus_code = {
     "200": 0,
     "301": 0,
@@ -13,22 +15,21 @@ satus_code = {
 
 file_size = 0
 
-while True:
-    try:
-        for x, line in enumerate(sys.stdin, 1):
-            sp_line = line.strip().split()
-            line_len = len(sp_line)
-            file_size += int(sp_line[line_len - 1])
-            status = sp_line[line_len - 2]
-            if status in satus_code:
-                satus_code[str(status)] += 1
-            if x % 10 == 0:
-                print("File size: {}".format(file_size))
-                for i in sorted(satus_code):
-                    if satus_code[i] > 0:
-                        print("{}: {}".format(i, satus_code[i]))
-    except KeyboardInterrupt:
-        pass
-    print('File size: {:d}'.format(file_size))
-    for key in sorted(satus_code.keys()):
-        print("{}: {:d}".format(key, satus_code[key]))
+try:
+    for x, line in enumerate(sys.stdin, 1):
+        sp_line = line.strip().split()
+        line_len = len(sp_line)
+        file_size += int(sp_line[line_len - 1])
+        status = sp_line[line_len - 2]
+        if status in satus_code:
+            satus_code[str(status)] += 1
+        if x % 10 == 0:
+            print("File size: {}".format(file_size))
+            for i in sorted(satus_code):
+                if satus_code[i] > 0:
+                    print("{}: {}".format(i, satus_code[i]))
+except KeyboardInterrupt:
+    pass
+print('File size: {:d}'.format(file_size))
+for key in sorted(satus_code.keys()):
+    print("{}: {:d}".format(key, satus_code[key]))
