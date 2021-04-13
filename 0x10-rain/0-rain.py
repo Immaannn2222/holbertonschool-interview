107dff2644a0f33facc05d2438670b6fd5124610
Immaannn2222 <imennaayari@gmail.com>
2021-04-13 12:05:47 +0100

    simulate to an area

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Rain
+"""
+
+
+def rain(walls):
+    """ calculate how much water will be retained after it rains"""
+    if len(walls) == 0:
+        return 0
+    retianed_water = 0
+    n_walls = len(walls)
+    for i in range(n_walls):
+        for j in range(i + 1, n_walls):
+            retianed_water = max(
+                retianed_water, min(walls[i], walls[j]) * (j - i - 1))
+    return retianed_water
