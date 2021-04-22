8dd308c8e829b13d35525933399f623e443607b2
Immaannn2222 <imennaayari@gmail.com>
2021-04-23 00:09:38 +0100

    push with the new prototype

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -6,7 +6,7 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void operate_heap(int arr[], int n, int i);
+void operate_heap(int arr[], int n, int i, size_t size);
 void swap(int *a, int *b);
 
 #endif
