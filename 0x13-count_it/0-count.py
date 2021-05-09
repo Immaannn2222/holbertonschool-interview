8d97e3019ab10a3a260fe6bb71602585ca46455d
Immaannn2222 <imennaayari@gmail.com>
2021-05-09 22:20:45 +0100

    try

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,8 +3,21 @@
 import requests
 
 
-def count_words(subreddit, word_list, after=""):
+def count_words(subreddit, word_list, count_dict={}, after=""):
     """recursive function that queries the Reddit API"""
+    if len(count_dict) <= 0:
+        for word in word_list:
+            count_dict[word.lower()] = 0
+
+    if after is None:
+        sorted_count_dict = dict(sorted(
+            count_dict.items(),
+            key=lambda x: (-x[1], x[0])
+            ))
+        for k, v in sorted_count_dict.items():
+            if v > 0:
+                print("{}: {}".format(k, v))
+        return None
     h = {'user-agent': 'xxxx'}
     url = 'https://api.reddit.com/r/{}/hot.json'.format(
         subreddit)
@@ -14,22 +27,17 @@ def count_words(subreddit, word_list, after=""):
     })
     if resp.status_code != 200:
         return None
-    try:
-        r = resp.json()
-    except Exception:
-        return None
-    titles = []
-    for post in r['data']['children']:
-        x = post['data']['title']
-        words = (x.lower()).split()
-        titles.append(words)
-    answer = {}
-    for item in word_list:
-        answer[item] = 0
-        if item in titles:
-            answer[item] += item.count()
-        else:
-            answer[item] = 0
-    for k, v in sorted(answer.items(), key=lambda x: (-x[1], x[0])):
-        print("{} = {}".format(k.lower(), v))
-    count_words(subreddit, word_list, after="")
+    else:
+        try:
+            r = resp.json()
+        except Exception:
+            return
+        after = resp.json().get("data").get("after")
+
+        for post in r['data']['children']:
+            x = post['data']['title']
+            words = (x.lower()).split()
+        for item in word_list:
+            count_dict[item.lower()] += words.count(
+                        item.lower())
+    count_words(subreddit, word_list, count_dict, after)
