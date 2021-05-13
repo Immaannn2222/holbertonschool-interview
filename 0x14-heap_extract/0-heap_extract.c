60e2678add89e1039620f3af1a2e6414d134f95c
Immaannn2222 <imennaayari@gmail.com>
2021-05-13 23:13:44 +0100

    add the static function

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,6 +1,47 @@
 #include "binary_trees.h"
 
 /**
+* heap_extract - extracts the root node of a Max Binary Heap
+* @root: double pointer to the root node of the heap
+*
+* Return: value stored in the root node or 0 on fail
+*/
+
+int heap_extract(heap_t **root)
+{
+int i, n, size;
+heap_t **array;
+
+if (!root || !*root)
+return (0);
+n = (*root)->n;
+size = get_size(*root);
+array = malloc(1024 * sizeof(heap_t *));
+if (!array)
+return (0);
+for (i = 0; i < size; i++)
+array[i] = NULL;
+arr_convert(array, *root, 0);
+if (array[size - 1] == (*root))
+{
+free(array);
+free(*root);
+*root = NULL;
+return (n);
+}
+(*root)->n = array[size - 1]->n;
+if (array[size - 1]->parent->left == array[size - 1])
+array[size - 1]->parent->left = NULL;
+else
+array[size - 1]->parent->right = NULL;
+free(array[size - 1]);
+free(array);
+reconstruct_heap(*root);
+return (n);
+}
+
+
+/**
  * get_size - returns the size of a heap
  * @root: the first node of the heap
  *
@@ -68,44 +109,4 @@ node->right->n = tempo;
 }
 reconstruct_heap(node->right);
 }
-}
-
-/**
-* heap_extract - extracts the root node of a Max Binary Heap
-* @root: double pointer to the root node of the heap
-*
-* Return: value stored in the root node or 0 on fail
-*/
-
-int heap_extract(heap_t **root)
-{
-int i, n, size;
-heap_t **array;
-
-if (!root || !*root)
-return (0);
-n = (*root)->n;
-size = get_size(*root);
-array = malloc(1024 * sizeof(heap_t *));
-if (!array)
-return (0);
-for (i = 0; i < size; i++)
-array[i] = NULL;
-arr_convert(array, *root, 0);
-if (array[size - 1] == (*root))
-{
-free(array);
-free(*root);
-*root = NULL;
-return (n);
-}
-(*root)->n = array[size - 1]->n;
-if (array[size - 1]->parent->left == array[size - 1])
-array[size - 1]->parent->left = NULL;
-else
-array[size - 1]->parent->right = NULL;
-free(array[size - 1]);
-free(array);
-reconstruct_heap(*root);
-return (n);
 }
