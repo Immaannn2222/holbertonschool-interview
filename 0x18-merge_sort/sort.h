5e413c7d394dc17dbfe726d1b77f8d7ffd9a39ab
Immaannn2222 <imennaayari@gmail.com>
2021-06-13 18:30:45 +0100

    forgot the prototype file

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_MERGE
+#define SORT_MERGE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
+#endif
