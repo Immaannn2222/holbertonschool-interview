f045d2af7084624a4398449737b01fad8f40e4ae
Immaannn2222 <imennaayari@gmail.com>
2021-07-28 19:32:57 +0100

    push to the repo

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* wildcmp - function that compares two strings
+* @s1: a pointer to the first string
+* @s2: a pointer to second string
+*
+* Return: 1 if the strings can be considered identical, otherwise return 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(++s1, ++s2));
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+return (0);
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,8 @@
+#ifndef RECUSRION_H
+#define RECUSRION_H
+
+#include <stdlib.h>
+
+int wildcmp(char *s1, char *s2);
+
+#endif
