f3f1c3229aeb0f6ed9a1d9f255a9030070dde038
Immaannn2222 <imennaayari@gmail.com>
2021-08-05 10:42:37 +0100

    add the 0 condition

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -6,6 +6,8 @@
 
 def pascal_triangle(n):
     """list of lists of integers representing the Pascal’s triangle of n"""
+    if n < 0:
+        return []
     line = []
     for i in range(n):
         x = 11 ** i
