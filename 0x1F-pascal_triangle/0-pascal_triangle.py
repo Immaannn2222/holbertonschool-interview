35dc28d8fc3df721df46b99e93efec4812980d1e
Immaannn2222 <imennaayari@gmail.com>
2021-08-05 11:12:30 +0100

    third time id the charm

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -6,12 +6,12 @@
 
 def pascal_triangle(n):
     """list of lists of integers representing the Pascal’s triangle of n"""
-    if n < 0:
+    if n <= 0:
         return []
-    line = []
+    res = []
     for i in range(n):
-        x = 11 ** i
-        line.append(x)
-    for j in range(0, len(line)):
-        line[j] = str(line[j])
-    return line
+        res.append([1] * (i + 1))
+    for i in range(2, n):
+        for j in range(1, i):
+            res[i][j] = res[i-1][j-1] + res[i-1][j]
+    return res
